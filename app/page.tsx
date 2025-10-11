31d57620022ceafb3e284298cefd9ba3dd5ba08f
lorenzo.benigni <>
2025-10-11 19:18:26 +0200

    Add console log for menuByCategory in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
     );
 
   console.log(menuByCategory);
+  console.log(menuByCategory);
   const categories = menuByCategory ? Object.keys(menuByCategory) : [];
 
   return (
