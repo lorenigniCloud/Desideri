73a9d003c2149803a3075bdcd06b68ab1f731869
lorenzo.benigni <>
2025-10-11 19:56:36 +0200

    try new resource

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
 
   console.log(menuByCategory);
   console.log(menuByCategory);
+  console.log(menuByCategory);
   const categories = menuByCategory ? Object.keys(menuByCategory) : [];
 
   return (
